0131ae2ee4ef54f64707bdc2ea9722577aeba6e6
candra saputra <candrasaputra@live.com>
2025-08-27 11:44:49 +0700

    text styling

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,11 @@ void main() {
             ),
           ),
           child: const Center(
-            child: Text('Hello World'),
+            child: Text('Hello World!', style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            )),
           ),
         ),
       ),
