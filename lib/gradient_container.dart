88ee2200ae07526ff07d815410a05bdf2e2758aa
candra saputra <candrasaputra@live.com>
2025-08-27 16:03:33 +0700

    color variable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,23 +5,23 @@ var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
-  Widget build(contex) {
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 95),
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
       child: const Center(
-        child: StyledText('Hello World!2'),
+        child: StyledText('Hello World!'),
       ),
     );
   }
