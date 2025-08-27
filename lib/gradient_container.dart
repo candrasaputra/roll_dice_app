564537fa91da8b3ccef560df81e95f404b18876a
candra saputra <candrasaputra@live.com>
2025-08-27 15:47:45 +0700

    var

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -6,25 +10,18 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(contex) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 95),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        child: StyledText('Hello World!2'),
       ),
     );
   }
