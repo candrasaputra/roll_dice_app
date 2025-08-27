3d4d9f7d667a151a8d89ccc471f0954e13fbb6e7
candra saputra <candrasaputra@live.com>
2025-08-27 16:18:58 +0700

    add imgaes asset

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 33, 5, 109),
-          Color.fromARGB(255, 68, 21, 149),
-        ),
+        body: GradientContainer.purple(),
       ),
     ),
   );
