20720bb3903783122f23298627fcb5f36dfa6eca
candra saputra <candrasaputra@live.com>
2025-08-28 11:16:33 +0700

    create statefullwidgate

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer.purple(),
+        // body: GradientContainer.purple(),
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+        ),
       ),
     ),
   );
